031a83d5f1f8a0d102f30470e531384efd5f9da6
jwlodek <jakub.wlodek@stonybrook.edu>
2020-04-29 14:41:39 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Reddit-Code-Block
+
+A simple firefox extension for allowing much easier typing of code blocks on the old reddit website.
+(In-Progress)
---
+++ reddit-code-block.js
@@ -0,0 +1,33 @@
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+function replaceSelectedText(replacementText) {
+    var sel, range;
+    if (window.getSelection) {
+        sel = window.getSelection();
+        if (sel.rangeCount) {
+            range = sel.getRangeAt(0);
+            range.deleteContents();
+            range.insertNode(document.createTextNode(replacementText));
+        }
+    } else if (document.selection && document.selection.createRange) {
+        range = document.selection.createRange();
+        range.text = replacementText;
+    }
+}
+
+document.addEventListener('keydown', function (event) {
+    if(event.keyCode === 9){
+      setTimeout(function() {
+        event.target.value += '    ';
+      }, 4);
+      event.preventDefault();
+    }
+});
